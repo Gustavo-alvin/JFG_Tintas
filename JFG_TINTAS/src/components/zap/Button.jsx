bcb992150de6359945f0a42704170ff5bbd9fbc6
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2025-01-29 22:00:04 -0300

    up

--- JFG_TINTAS/src/components/zap/Button.jsx
+++ JFG_TINTAS/src/components/zap/Button.jsx
@@ -4,7 +4,7 @@ import './button.css'
 const Button = () => {
   return (
     <div>
-        <button className='btn-zap'><a href="https://wa.me/11914250648"><i className="fa fa-whatsapp"></i> Fale Conosco </a></button>
+        <button className='btn-zap'><a href="https://wa.me/11964890033"><i className="fa fa-whatsapp"></i> Fale Conosco </a></button>
     </div>
   )
 }
